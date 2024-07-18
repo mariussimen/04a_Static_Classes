dfc28c41ecc808d868609beb320c9ee6c93bec59
mariussimen <theodores03@yahoo.fr>
2024-07-18 12:25:24 +0200

    update

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -6,21 +6,21 @@ public class App {
 	
 	public static void main(String[] args) {
 		
-		// RechnerInst rechnerInst = new RechnerInst();
+		RechnerInst rechnerInst = new RechnerInst();
 		
-		// output("Ergebnis: " + rechnerInst.summe(1, 1));
-		// output("Ergebnis: " + rechnerInst.differenz(5, 1));
-		// output("Ergebnis: " + rechnerInst.produkt(2, 3));
-		// output("Ergebnis: " + rechnerInst.quotient(2, 4));
+		output("Ergebnis: " + rechnerInst.summe(1, 1));
+		output("Ergebnis: " + rechnerInst.differenz(5, 1));
+		output("Ergebnis: " + rechnerInst.produkt(2, 3));
+		output("Ergebnis: " + rechnerInst.quotient(2, 4));
 		
 		output("-------------------------");
 
-		// output(preStr + RechnerStat.summe(1, 1));
-		// output(preStr + RechnerStat.differenz(5, 1));
-		// output(preStr + RechnerStat.produkt(2, 3));
-		// output(preStr + RechnerStat.quotient(2, 4));
+		output(preStr + RechnerStat.summe(1, 1));
+		output(preStr + RechnerStat.differenz(5, 1));
+		output(preStr + RechnerStat.produkt(2, 3));
+		output(preStr + RechnerStat.quotient(2, 4));
 		
-		// output(preStr + Math.PI);
+		output(preStr + Math.PI);
 		
 	}
 
